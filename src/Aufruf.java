6cf46d78e7ed41fa4342723a0ba11495dd675789
Your Name <you@example.com>
2019-01-28 21:30:16 +0100

    Aufga 3

--- src/Aufruf.java
+++ src/Aufruf.java
@@ -2,9 +2,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
 
 public class Aufruf {
     public static void main(String[] args) throws Exception
@@ -12,13 +11,18 @@ public class Aufruf {
 
         ObjectMapper objectMapper=new ObjectMapper();
         Merge a = objectMapper.readValue(new File("src/merge.json"), Merge.class);
-        System.out.println(a.toString());
         //marskall
         JAXBContext jaxbContext     = JAXBContext.newInstance( Merge.class );
         Marshaller jaxbMarshaller   = jaxbContext.createMarshaller();
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        OutputStream os = new FileOutputStream( "employee.xml" );
+        OutputStream os = new FileOutputStream( "merge.xml" );
         jaxbMarshaller.marshal( a, os );
+        //unMarskall
+        InputStream is = new FileInputStream("merge.xml");
+        Unmarshaller unmarshallerObj = jaxbContext.createUnmarshaller();
+        Merge rA=(Merge) unmarshallerObj.unmarshal(is);
+        //reJson
+        System.out.println(objectMapper.writeValueAsString(rA));
 
 
 
